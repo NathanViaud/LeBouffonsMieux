b3abfd34b44ba0b5154ae3d362b436a8378f84f1
VIAUD Nathan <nathan.viaud@inria.fr>
2024-04-02 11:22:05 +0200

    chore: add router

---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            redirect: '/search'
+        },
+        {
+            path: '/search',
+            component: () => import('@/pages/search.vue')
+        }
+    ]
+})
