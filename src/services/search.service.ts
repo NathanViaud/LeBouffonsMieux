110313dba71680e237c6a6840280e92c12921805
VIAUD Nathan <nathan.viaud@inria.fr>
2024-04-03 10:30:13 +0200

    fix: remove API Key

--- src/services/search.service.ts
+++ src/services/search.service.ts
@@ -1,7 +1,7 @@
 import type { SearchResult } from '@/types';
 import axios from 'axios';
 
-const API_KEY='T2U4MjLqQ95lMhBvnPSDVgb7emXjxGwzBZqWZEdu';
+const API_KEY= import.meta.env.VITE_API_KEY;
 const API_URL = 'https://api.nal.usda.gov/fdc/v1/foods/search';
 
 export async function search(query: string): Promise<SearchResult[]> {
