f3359ac4f44bb9e8a1756d3002c677becbe7e28e
VIAUD Nathan <nathan.viaud@inria.fr>
2024-04-04 15:28:10 +0200

    style: hiding search bar when not on route search

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,9 @@ import { Button } from '@/components/ui/button';
 import { Search } from 'lucide-vue-next'
 import { router } from '@/router.ts';
 import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 
 const emit = defineEmits<{
     (e: 'search', query: string) : void;
@@ -20,15 +23,15 @@ function handleSearch() {
 <template>
     <div class="border-b-2 border-b-gray-200 pt-2 pb-3 px-3 align-center bg w-full fixed z-10 top-0 backdrop-blur-lg">
         <div class="flex flex-row">
-            <router-link to="/" class="flex flex-row gap-2 flex-1">
+            <router-link to="/" class="flex flex-row gap-2">
                 <img class="h-24 rounded-xl" src="@/assets/DALLE_2024-04-02_12.06.59_-_Create_a_logo_for_an_application_named_LeBouffonsMieux._The_logo_should_combine_the_imagery_of_a_jester_bouffon_and_food_creating_a_playful_yet_r.webp" alt="">
                 <div class="text-3xl font-bold flex flex-col justify-center">
                     <p>Bouffons</p>
                     <p>Mieux</p>
                 </div>
             </router-link>
-            <div class="flex flex-row items-center gap-6 ">
-                <div class="relative w-full max-w-sm items-center">
+            <div class="flex flex-1 justify-end flex-row items-center gap-6 ">
+                <div v-if="route.path === '/search'" class="relative w-full max-w-sm items-center">
                     <Input v-model="search" placeholder="bread" @keydown.enter="handleSearch" class="pl-10" />
                     <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
                         <Search class="size-6 text-muted-foreground" />
