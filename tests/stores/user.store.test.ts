e7e098d75c9f8d7fe3e83ebaeabbe8c6b5e9185a
VIAUD Nathan <nathan.viaud@inria.fr>
2024-04-04 17:01:12 +0200

    test: userStore getters

--- tests/stores/user.store.test.ts
+++ tests/stores/user.store.test.ts
@@ -28,6 +28,67 @@ describe('User store', () => {
         userStore.addFood(breadFood);
         userStore.addFood(cheddarFood);
         
-        expect(userStore.getEnergy).toBe(662 * 2);
+        expect(userStore.getEnergy).toBe(254 * 2 + 408 * 2);
+    })
+    
+    it('getWater', async() => {
+        const userStore = useUserStore();
+        
+        const breadFood = createFood(bread, 200);
+        const cheddarFood = createFood(cheddar, 200);
+        
+        userStore.addFood(breadFood);
+        userStore.addFood(cheddarFood);
+        
+        expect(userStore.getWater).toBe(36.6 * 2 + 38.7 * 2);
+    })
+    
+    it('getCarbohydrate', async() => {
+        const userStore = useUserStore();
+        
+        const breadFood = createFood(bread, 200);
+        const cheddarFood = createFood(cheddar, 200);
+        
+        userStore.addFood(breadFood);
+        userStore.addFood(cheddarFood);
+        
+        expect(userStore.getCarbohydrate).toBe(2.44 * 2 + 43.1 * 2);
+    })
+    
+    it('getLipid', async() => {
+        const userStore = useUserStore();
+        
+        const breadFood = createFood(bread, 200);
+        const cheddarFood = createFood(cheddar, 200);
+        
+        userStore.addFood(breadFood);
+        userStore.addFood(cheddarFood);
+        
+        expect(userStore.getLipid).toBe(3.55 * 2 + 34 * 2);
+    })
+    
+    it('getProtein', async() => {
+        const userStore = useUserStore();
+        
+        const breadFood = createFood(bread, 200);
+        const cheddarFood = createFood(cheddar, 200);
+        
+        userStore.addFood(breadFood);
+        userStore.addFood(cheddarFood);
+        
+        expect(userStore.getProtein).toBe(12.3 * 2 + 23.3 * 2);
+    
+    })
+    
+    it('getAlcohol', async() => {
+        const userStore = useUserStore();
+        
+        const breadFood = createFood(bread, 200);
+        const cheddarFood = createFood(cheddar, 200);
+        
+        userStore.addFood(breadFood);
+        userStore.addFood(cheddarFood);
+        
+        expect(userStore.getAlcohol).toBe(0);
     })
 })
