8e0931e578f54dcbeef9911f4acbbba4e9d47228
Julien-Clerc <julien.clerc@epitech.eu>
2024-04-04 11:40:44 +0200

    Feat add result table

---
+++ src/components/DailyList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { Table, TableRow, TableHeader, TableHead, TableBody, TableCell } from '@/components/ui/table';
+
+defineProps<{
+    dataFood: {
+        Name: string
+        Amount: Number
+    }[]
+}>()
+
+</script>
+
+<template>
+    <div class="">
+        <Table>
+        <TableHeader>
+            <TableRow>
+                <TableHead>Food</TableHead>
+                <TableHead>Amount</TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            <TableRow v-for="item in dataFood">
+                <TableCell class="font-bold">{{ item.Name }}</TableCell>
+                <TableCell>{{ item.Amount }}</TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
+    </div>
+</template>
---
+++ src/components/DailyResult.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Table, TableRow, TableHeader, TableHead, TableBody, TableCell } from '@/components/ui/table';
+
+defineProps<{
+    data: {
+        Name: string
+        Amount: Number
+        Unit: String
+        Recommendation: Number
+    }[]
+}>()
+
+</script>
+
+<template>
+    <div class="">
+        <Table>
+        <TableHeader>
+            <TableRow>
+                <TableHead>Nutriments</TableHead>
+                <TableHead>Average Amount</TableHead>
+                <TableHead>Unit</TableHead>
+                <TableHead>Daily Recommendation</TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            <TableRow v-for="item in data">
+                <TableCell class="font-bold">{{ item.Name }}</TableCell>
+                <TableCell>{{ item.Amount }}</TableCell>
+                <TableCell>{{ item.Unit }}</TableCell>
+                <TableCell>{{ item.Recommendation }}</TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
+    </div>
+</template>
